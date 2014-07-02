5bbddada2ff01d6607ed9dae309b4c9157007d09
Beyang Liu <beyang.liu@gmail.com>
2014-07-02 21:53:48 +0000

    import reference multi-part qnames

--- src/main/java/org/yinwang/pysonar/ast/Import.java
+++ src/main/java/org/yinwang/pysonar/ast/Import.java
@@ -27,31 +27,38 @@ public class Import extends Node {
     @Override
     public Type transform(@NotNull State s) {
         for (Alias a : names) {
-            Type mod = Analyzer.self.loadModule(a.name, s);
-            if (mod == null) {
-                Analyzer.self.putProblem(this, "Cannot load module");
-                continue;
-            }
-            if (a.asname != null) {
-                // As-name binding
-                // Note: this won't be outputted by JSONDump, because it has same key as original definition.
-                // Alternatively, we could treat this as its own definition, but instead, treat this as just another
-                // reference to the original definition (all references to this as-name point to the original
-                // definition, too)
-                s.insert(a.asname.id, a.asname, mod, Binding.Kind.VARIABLE);
-            }
-            // module reference
-            if (mod instanceof ModuleType) {
-                Set<Binding> bs = Analyzer.self.moduleTable.lookup(((ModuleType)mod).qname);
-                if (bs != null) {
-                    // As-name
-                    if (a.asname != null) {
-                        Analyzer.self.putRef(a.asname, bs);
+            // module and ancestor modules
+            for (int i = 1; i <= a.name.size(); i++) {
+                List<Name> qname = a.name.subList(0, i);
+                Type mod = Analyzer.self.loadModule(qname, s);
+                if (mod == null) {
+                    if (a.name.size() == qname.size()) {
+                        Analyzer.self.putProblem(this, "Cannot load module");
                     }
+                    continue;
+                }
+                if (a.name.size() == qname.size()) {
+                    if (a.asname != null) {
+                        // As-name binding Note: this won't be outputted by JSONDump, because it has same key as
+                        // original definition. Alternatively, we could treat this as its own definition, but instead,
+                        // treat this as just another reference to the original definition (all references to this
+                        // as-name point to the original definition, too)
+                        s.insert(a.asname.id, a.asname, mod, Binding.Kind.VARIABLE);
+                    }
+                }
 
-                    // Last-name
-                    Analyzer.self.putRef(a.name.get(a.name.size()-1), bs);
-                    // TODO: do previous names
+                // module reference
+                if (mod instanceof ModuleType) {
+                    Set<Binding> bs = Analyzer.self.moduleTable.lookup(((ModuleType)mod).qname);
+                    if (bs != null) {
+                        if (a.name.size() == qname.size() && a.asname != null) {
+                            // As-name
+                            Analyzer.self.putRef(a.asname, bs);
+                        }
+
+                        // Name
+                        Analyzer.self.putRef(qname.get(qname.size()-1), bs);
+                    }
                 }
             }
         }
